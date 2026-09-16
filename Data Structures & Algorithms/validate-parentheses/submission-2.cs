88254e29b4298314b4cca6a8d9ede19804bd3fd4
Shivanib03 <66016175+Shivanib03@users.noreply.github.com>
2026-09-16 23:25:17 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.cs
@@ -0,0 +1,36 @@
+public class Solution {
+    public bool IsValid(string s) {
+        // var stk = new Stack<int>();
+        // for(int i = 0; i < s.Length; i++)
+        // {
+        //     if(stk.Count > 0 &&
+        //         (stk.Peek() == '(' && s[i] == ')' ||
+        //         stk.Peek() == '{' && s[i] == '}' ||
+        //         stk.Peek() == '[' && s[i] == ']'))
+        //         stk.Pop();
+        //     else
+        //         stk.Push(s[i]);
+        // }
+        // return stk.Count == 0;
+
+        Stack<char> stack = new Stack<char>();
+        Dictionary<char, char> closeToOpen = new Dictionary<char, char> {
+            { ')', '(' },
+            { ']', '[' },
+            { '}', '{' }
+        };
+
+        foreach (char c in s) {
+            if (closeToOpen.ContainsKey(c)) {
+                if (stack.Count > 0 && stack.Peek() == closeToOpen[c]) {
+                    stack.Pop();
+                } else {
+                    return false;
+                }
+            } else {
+                stack.Push(c);
+            }
+        }
+        return stack.Count == 0;        
+    }
+}
